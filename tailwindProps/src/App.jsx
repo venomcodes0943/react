040cd472b67aa6e96d53c14f0571ffc173b48634
venomcodes0943 <dhcodes0943@gmail.com>
2024-03-27 14:23:05 +0500

    Adding a Tailwind project

---
+++ tailwindProps/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="bg-red-800 p-4 rounded-sm">TailWind classNamees</h1>
+    </>
+  )
+}
+
+export default App
---
+++ tailwindProps/src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import Card from './components/Card.jsx'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+    <Card newVale="Chai Aur React" />
+    <Card />
+  </React.StrictMode >,
+)
