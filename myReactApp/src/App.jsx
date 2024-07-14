e83e2b71ee66b0c2c7084113eec6d806e8f58381
venomcodes0943 <dhcodes0943@gmail.com>
2024-07-14 17:04:39 +0500

    Starting with myReactApp

---
+++ myReactApp/src/App.jsx
@@ -0,0 +1,11 @@
+// import react from 'react'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-center text-3xl font-bold mt-4">Welcome to React</h1>
+    </>
+  )
+}
+
+export default App
