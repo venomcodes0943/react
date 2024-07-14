354c24bffa8d8de420c0b3f6dbe09e5da0234588
venomcodes0943 <dhcodes0943@gmail.com>
2024-07-14 15:39:01 +0500

    New Starting

--- myreact/src/newApp.jsx
+++
@@ -1,7 +0,0 @@
-function NewApp() {
-    return (
-        <p className="Para">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam placeat quod animi, alias facere aliquid temporibus accusamus error qui earum, adipisci architecto exercitationem impedit inventore laudantium magnam ipsa? Eius, eveniet?</p>
-    )
-}
-
-export default NewApp
